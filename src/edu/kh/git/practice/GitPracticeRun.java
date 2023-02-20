678096d6f59bf3c4e7e475657106a7823479e5d2
Huynjeong Jeon <jhj.sharon@gmail.com>
2023-02-20 20:17:06 +0900

    gitjhub 연동완료

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -5,6 +5,7 @@ public class GitPracticeRun {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요");
 		System.out.println("안녕하세요");
+		System.out.println("github 연동완료");
 		
 	}
 
